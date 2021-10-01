86178d7afd9800e306eacb30a170fddcdf39c866
Mert Ertemur <mertertemur@gmail.com>
2021-10-01 22:57:46 +0300

    organizing
    organizing-code-and-readme

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # gartic.io-bot
-Self-drawing in-game bot for gartic.io
-This project's purpose is not to create a cheat-bot or an evil 3rd party program. It's all about learning and practicing.
+ Self-drawing in-game bot for gartic.io
+ This project's purpose is not to create a cheat-bot or an evil 3rd party program. It's all about learning and practicing.
+ 
+# Dependencies
+ - You need to use pytesseract. Here you can find the recommended version: https://github.com/UB-Mannheim/tesseract/wiki
+ - You need to download chromedriver. Here you can find the versions: https://chromedriver.chromium.org/
+ - You need to install these via command: pip install
+   - selenium
+   - opencv
+   - pillow
+   - pytesseract
+   - pyautogui
+
+# How-to-use
+ After fulfilling all the mentioned requirements, you just need to open gartic.io on fullscreen at the first tab. When it's your turn to draw, you can run the code and it should do the trick.
+ There can be mistakes or necessary improvements. I'm open to ideas.
--- gartic.py
+++ gartic.py
@@ -9,16 +9,20 @@ from pytesseract import pytesseract
 import pyautogui as pa
 
 
-
-pytesseract.tesseract_cmd= "D:\\Uygulamalar\\tes\\tesseract.exe"
+#click left-top corner. (need to open the game  fulscreen-firsttab)
 pa.FAILSAFE = False
 pa.click(0,0)
+
+#take a pic of the words' box
 img = ImageGrab.grab(bbox=(950,200,1350,280))
 img_np = np.array(img)
 frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+
+#turn image to string       ** YOU NEED TO CHANGE THE PATH
+pytesseract.tesseract_cmd= "D:\\Uygulamalar\\tes\\tesseract.exe"
 word = pytesseract.image_to_string(frame)
 word = word.upper()
-print(word)
+#get rid of unneccessary/failure parts. could be extended
 word =word.replace("HINT", "")
 word =word.replace("SKIP", "")
 word =word.replace("-E", "")
@@ -34,35 +38,31 @@ word =word.replace("!", "I")
 word =word.replace("\n", "")
 word =word.strip()
 
-print(word)
-ser = word + " black-and-white 2d"
-print(ser)
+#the search word that we searh for on chrome
+ser = word + " vector 2D"
 
-
-
-
-
+#open a chrome tab. search for images. download first one. **YOU NEED TO CHANGE THE PATH
 driver = webdriver.Chrome('D:\Yazilim\OpenCV\gartic\chromedriver.exe')
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver.get('https://www.google.com.tr/imghp?hl=tr&ogbl')
 box = driver.find_element_by_xpath('//*[@id="sbtc"]/div/div[2]/input')
-box.send_keys(word + " vector 2D")
+box.send_keys(ser)
 box.send_keys(Keys.ENTER)
 driver.find_element_by_xpath('//*[@id="yDmH0d"]/div[2]/c-wiz/div[1]/div/div[1]/div[2]/div/div/div').click()
-time.sleep(0.1)
+time.sleep(0.1) # ^^ tools ^^
 driver.find_element_by_xpath('//*[@id="yDmH0d"]/div[2]/c-wiz/div[2]/div[2]/c-wiz[1]/div/div/div[1]/div/div[1]').click()
-time.sleep(0.1)
+time.sleep(0.1) # ^^ size ^^
 driver.find_element_by_xpath('//*[@id="yDmH0d"]/div[2]/c-wiz/div[2]/div[2]/c-wiz[1]/div/div/div[3]/div/a[4]/div').click()
-time.sleep(0.1)
+time.sleep(0.1) # ^^ icon ^^
 driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[1]').click()
-time.sleep(0.1)
+time.sleep(0.1) # ^^ the first photo ^^
 driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[1]/a[1]/div[1]/img').screenshot('D:\Yazilim\OpenCV\gartic\hop.png')
-driver.close()
+driver.close() # ^^ screenshot/download ^^ (you may want to change the path)
 driver.quit()
 
 time.sleep(1)
-try:
+try: #load image. as a precaution to different diletypes
     img_found = cv2.imread("hop.png")
 except:
     try:
@@ -70,11 +70,12 @@ except:
     except:
         print("no img")
         pass
-img_found = cv2.resize(img_found,(120,120))
-#img_found = cv2.blur(img_found,(3,3),0)
+img_found = cv2.resize(img_found,(120,120)) 
+#img_found = cv2.blur(img_found,(3,3),0) # optional but not optimal for small pics
 ret,img_found = cv2.threshold(img_found,127,255,cv2.THRESH_BINARY_INV)
 img_found = cv2.Canny(img_found,40,150)
-gap = 6
+# to prevent it from draw straight lines. bcs it take too much time
+gap = 6 
 lastPoint = (0,0)
 for i in range(img_found.shape[0]-1):
     for k in range(img_found.shape[1]-1):
@@ -83,7 +84,7 @@ for i in range(img_found.shape[0]-1):
                 pa.click(x=950+k, y=320+i)
                 lastPoint = (950+k, 320+i)
 
-
+#delete the old file.
 try:
     os.remove("hop.jpg")
 except:
